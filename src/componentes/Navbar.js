1c16dfd4ac52744f41ed2933d94702a2af0bcce4
Micaela Romero <rom_micaela@hotmail.com>
2023-02-16 19:44:49 -0300

    logo al navbar

---
+++ src/componentes/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Navbar(){
+    return(
+        <nav>
+          <div className = "menu-logo">
+           <img clasName = "menu-logo"src={ require ( '../img/logo_chico.jpg')}/>
+          </div>
+        </nav>
+        );
+      }
+
+
+export default Navbar;
+
