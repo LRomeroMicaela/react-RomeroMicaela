f02fac2cc1b8b9eff108306f2e14802d62847636
Micaela Romero <rom_micaela@hotmail.com>
2023-02-15 21:19:12 -0300

    intento 1 navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from './componentes/Navbar.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
     </div>
   );
 }
