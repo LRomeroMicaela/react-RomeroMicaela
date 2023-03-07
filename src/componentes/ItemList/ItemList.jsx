47890cc37ea6833b20f6f8647bc591e8ab14f1b8
Micaela Romero <rom_micaela@hotmail.com>
2023-03-07 20:33:25 -0300

    ppio item

---
+++ src/componentes/ItemList/ItemList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Item from "../Item/Item";
+
+const ItemList = ({ items }) => {
+  return (
+    <div>
+      {items.map((elemento) => {
+        return <Item key={elemento.id} elemento={elemento} />;
+      })}
+    </div>
+  );
+};
+
+export default ItemList;
+
+//se importa acá item, que es el código con la estructura de la card
+// y se exporta itemList a itemListContainer, acá se hace solo el mapeo
+// se renderiza cada card en itemlist.
+//se le pasa cada elemento al hijo item a través de elemento
+//para que cada producto se mapea con un id distinto se usa element.id
+//
--- src/componentes/ItemListContainer/ItemListContainer.jsx
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -1,12 +1,34 @@
-import React from "react";
-import "./ItemListContainer.modules.css";
+import { useState, useEffect } from "react";
+import { products } from "../../productsMock";
+import styles from "./ItemListContainer.module.css";
+import ItemList from "../ItemList/ItemList";
 
-const ItemListContainer = (greeting) => {
+const ItemListContainer = ({ mensaje }) => {
+  const [items, setItems] = useState([]);
+  //items donde se guarda productos, setItems es la funcion que se usa para guardar los productos en items
+  useEffect(() => {
+    const listaProductos = new Promise((resolve, reject) => {
+      resolve(products);
+    });
+
+    listaProductos.then((ok) => {
+      setItems(ok);
+    });
+    listaProductos.catch((error) => {
+      console.log(error);
+    });
+    //acá se resuelve con ok(guardando los producto en el arreglo items)
+    // o se ejecuta el error si no se puede resolver la promesa
+  }, []);
   return (
     <div>
-      <h2 className="mensajeEstilo">{greeting.mensaje}</h2>
+      <h2 className={styles.mensajeEstilo}>{mensaje}</h2>
+      <ItemList items={items} />
     </div>
   );
 };
-
+//le paso el array de productos (items) a itemList, donde se mapea cada card
 export default ItemListContainer;
+
+//se importa itemList dentro del return
+//me falta setTimeOut de 2 segundos... ver!
